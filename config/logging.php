4660fcddad0ebf0da11af566c1a4e2cd9f4b8533
Nguyen Chau Thach <thachnc2002@gmail.com>
2023-05-18 14:42:07 +0700

    logging n vercel

--- config/logging.php
+++ config/logging.php
@@ -50,27 +50,111 @@ return [
     |
     */
 
+    // 'channels' => [
+    //     // 'stack' => [
+    //     //     'driver' => 'stack',
+    //     //     'channels' => ['single','vercel'],
+    //     //     'ignore_exceptions' => false,
+    //     // ],
+    //     'stack' => [
+    //         'driver' => 'stack',
+    //         'channels' => ['stderr'],
+    //     ],
+
+    //     'single' => [
+    //         'driver' => 'single',
+    //         'path' => storage_path('logs/laravel.log'),
+    //         'level' => env('LOG_LEVEL', 'debug'),
+    //     ],
+
+    //     'daily' => [
+    //         'driver' => 'daily',
+    //         'path' => storage_path('logs/laravel.log'),
+    //         'level' => env('LOG_LEVEL', 'debug'),
+    //         'days' => 14,
+    //     ],
+
+    //     'slack' => [
+    //         'driver' => 'slack',
+    //         'url' => env('LOG_SLACK_WEBHOOK_URL'),
+    //         'username' => 'Laravel Log',
+    //         'emoji' => ':boom:',
+    //         'level' => env('LOG_LEVEL', 'critical'),
+    //     ],
+
+    //     'papertrail' => [
+    //         'driver' => 'monolog',
+    //         'level' => env('LOG_LEVEL', 'debug'),
+    //         'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
+    //         'handler_with' => [
+    //             'host' => env('PAPERTRAIL_URL'),
+    //             'port' => env('PAPERTRAIL_PORT'),
+    //             'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+    //         ],
+    //     ],
+
+    //     // 'stderr' => [
+    //     //     'driver' => 'monolog',
+    //     //     'level' => env('LOG_LEVEL', 'debug'),
+    //     //     'handler' => StreamHandler::class,
+    //     //     'formatter' => env('LOG_STDERR_FORMATTER'),
+    //     //     'with' => [
+    //     //         'stream' => 'php://stderr',
+    //     //     ],
+    //     // ],
+
+    //     'stderr' => [
+    //         'driver' => 'monolog',
+    //         'handler' => Monolog\Handler\StreamHandler::class,
+    //         'with' => [
+    //             'stream' => 'php://stderr',
+    //         ],
+    //     ],
+
+    //     'syslog' => [
+    //         'driver' => 'syslog',
+    //         'level' => env('LOG_LEVEL', 'debug'),
+    //     ],
+
+    //     'errorlog' => [
+    //         'driver' => 'errorlog',
+    //         'level' => env('LOG_LEVEL', 'debug'),
+    //     ],
+
+    //     'null' => [
+    //         'driver' => 'monolog',
+    //         'handler' => NullHandler::class,
+    //     ],
+
+    //     'emergency' => [
+    //         'path' => storage_path('logs/laravel.log'),
+    //     ],
+
+    //     'vercel' => [
+    //         'driver' => 'errorlog',
+    //         'level' => 'debug',
+    //     ],
+    // ],   
+
+
+
     'channels' => [
-        // 'stack' => [
-        //     'driver' => 'stack',
-        //     'channels' => ['single','vercel'],
-        //     'ignore_exceptions' => false,
-        // ],
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['stderr'],
+            'channels' => ['single'],
+            'ignore_exceptions' => false,
         ],
 
         'single' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
         ],
 
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
             'days' => 14,
         ],
 
@@ -79,33 +163,23 @@ return [
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
             'username' => 'Laravel Log',
             'emoji' => ':boom:',
-            'level' => env('LOG_LEVEL', 'critical'),
+            'level' => 'critical',
         ],
 
         'papertrail' => [
             'driver' => 'monolog',
-            'level' => env('LOG_LEVEL', 'debug'),
-            'handler' => env('LOG_PAPERTRAIL_HANDLER', SyslogUdpHandler::class),
+            'level' => 'debug',
+            'handler' => SyslogUdpHandler::class,
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
             ],
         ],
 
-        // 'stderr' => [
-        //     'driver' => 'monolog',
-        //     'level' => env('LOG_LEVEL', 'debug'),
-        //     'handler' => StreamHandler::class,
-        //     'formatter' => env('LOG_STDERR_FORMATTER'),
-        //     'with' => [
-        //         'stream' => 'php://stderr',
-        //     ],
-        // ],
-
         'stderr' => [
             'driver' => 'monolog',
-            'handler' => Monolog\Handler\StreamHandler::class,
+            'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
             'with' => [
                 'stream' => 'php://stderr',
             ],
@@ -113,12 +187,12 @@ return [
 
         'syslog' => [
             'driver' => 'syslog',
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
         ],
 
         'errorlog' => [
             'driver' => 'errorlog',
-            'level' => env('LOG_LEVEL', 'debug'),
+            'level' => 'debug',
         ],
 
         'null' => [
@@ -129,10 +203,5 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
-
-        'vercel' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
-        ],
-    ],   
+    ],
 ];
