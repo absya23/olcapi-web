6c597218cfc816b1006ba41f08532f2f4f0bbb2b
Nguyen Chau Thach <thachnc2002@gmail.com>
2023-05-23 13:53:25 +0700

    change config app.php

--- config/app.php
+++ config/app.php
@@ -55,7 +55,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://olcapi-web.vercel.app'),
+
 
     'asset_url' => env('ASSET_URL'),
 
